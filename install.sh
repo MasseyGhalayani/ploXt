a02f3f1d9f56055a121ad29d4511157611616ac3
massey ghalayani <massey.ghalayani@valeo.com>
2025-07-09 12:38:54 +0200

    requirements.txt added, and env got cleaned

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-# tested for python 3.8
-# conda create --name LineFormer python=3.8
+# tested for python 3.9
+# conda create --name ploXt python=3.9
 pip install openmim
 pip install chardet
 conda install pytorch==1.13.1 torchvision==0.14.1 pytorch-cuda=11.7 -c pytorch -c nvidia
@@ -12,3 +12,4 @@ pip install scipy==1.9.3
 pip install -e mmdetection
 pip install bresenham
 pip install tqdm
+pip intall -r requirements.txt
